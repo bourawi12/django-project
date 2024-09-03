2652e276c542a655444a5391df52a764db70f10a
bourawi12 <mohamed12bouaziz@gmail.com>
2024-09-03 17:39:12 +0100

    adding location data to profile

--- src/users/models.py
+++ src/users/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
+from localflavor.tn.forms import GOVERNORATE_CHOICES  # Update this import path as necessary
+
+class Location(models.Model):
+    address_1 = models.CharField(max_length=128, blank=True)
+    address_2 = models.CharField(max_length=128, blank=True)
+    city = models.CharField(max_length=84)
+    state = models.CharField(max_length=20, choices=GOVERNORATE_CHOICES, default='sfax')
+    
+    def __str__(self):
+        return f'{self.city}, {self.state}'
+
 
 class Profile(models.Model):
-    user= models.OneToOneField(User, on_delete=models.CASCADE)
-    photo = models.ImageField(null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo = models.ImageField(null=True, blank=True)
     bio = models.CharField(max_length=140, blank=True)
-    phone_number = models.CharField(max_length=12,blank=True)
+    phone_number = models.CharField(max_length=12, blank=True)
+    location = models.OneToOneField(Location,on_delete=models.CASCADE,null=True)
     
     def __str__(self):
         return f'{self.user.username}\'s Profile'
-        
