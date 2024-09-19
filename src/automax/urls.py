e3524205c5e9a7dd91c752386fb4dd0697fa2697
bourawi12 <mohamed12bouaziz@gmail.com>
2024-09-19 18:12:06 +0100

    Working with Meida files in Django

--- src/automax/urls.py
+++ src/automax/urls.py
@@ -15,12 +15,19 @@ Including another URLconf:
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
 from main import urls as main_app_urls
 
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(main_app_urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
