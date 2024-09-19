11ae0db6a91cb6474f788442582442238837152a
bourawi12 <mohamed12bouaziz@gmail.com>
2024-09-20 00:49:54 +0100

    class based views

--- src/users/views.py
+++ src/users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django.views import View
 
 def login_view(request):
     if request.method == 'POST':
@@ -13,20 +14,24 @@ def login_view(request):
             
             if user is not None:
                 login(request, user)  # Log the user in
-                messages.success(request,f'you are now logges as {username}')
+                messages.success(request, f'You are now logged in as {username}')
                 return redirect('home')  # Redirect to home page after successful login
             else:
-                # This case is actually handled by the form's internal validation, so no need to handle it manually.
-                messages.error(request,f'unable to login')
+                messages.error(request, 'Unable to login')
         else:
-            messages.error(request,f'unable to login')
-        # If form is not valid, or credentials are wrong, re-render the form with errors
+            messages.error(request, 'Unable to login')
+
+        # Re-render the form with errors
         return render(request, 'views/login.html', {'login_form': login_form})
 
     else:  # For a GET request
         login_form = AuthenticationForm()
         return render(request, 'views/login.html', {'login_form': login_form})
 
-def register_view(request):
-    register_form=UserCreationForm()
-    return render(request,'views/register.html',{'register_form':register_form})
+class RegisterView(View):
+    def get(self, request):
+        register_form = UserCreationForm()
+        return render(request, 'views/register.html', {'register_form': register_form})
+    
+    def post(self,request):
+        pass
