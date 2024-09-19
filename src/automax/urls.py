e915ebf6b4021c435305af92eec3b699f29b4cdc
bourawi12 <mohamed12bouaziz@gmail.com>
2024-09-19 19:01:43 +0100

    adding login view

--- src/automax/urls.py
+++ src/automax/urls.py
@@ -20,12 +20,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 from main import urls as main_app_urls
-
+from users import urls as users_app_urls
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(main_app_urls)),
+    path('',include(users_app_urls))
 ]
 
 if settings.DEBUG:
