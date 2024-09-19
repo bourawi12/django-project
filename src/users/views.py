7d397c237deb5697ebe259e115adf12160c119dc
bourawi12 <mohamed12bouaziz@gmail.com>
2024-09-20 00:04:40 +0100

    working with django messages

--- src/users/views.py
+++ src/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 def login_view(request):
     if request.method == 'POST':
@@ -12,11 +13,13 @@ def login_view(request):
             
             if user is not None:
                 login(request, user)  # Log the user in
+                messages.success(request,f'you are now logges as {username}')
                 return redirect('home')  # Redirect to home page after successful login
             else:
                 # This case is actually handled by the form's internal validation, so no need to handle it manually.
-                pass
-        
+                messages.error(request,f'unable to login')
+        else:
+            messages.error(request,f'unable to login')
         # If form is not valid, or credentials are wrong, re-render the form with errors
         return render(request, 'views/login.html', {'login_form': login_form})
 
