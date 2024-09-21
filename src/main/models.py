9624a11d2f9e75ea6a4da3562f266bf655d3bca5
bourawi12 <mohamed12bouaziz@gmail.com>
2024-09-21 01:23:50 +0100

    listing model

--- src/main/models.py
+++ src/main/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 import uuid
-
-from users.models import Profile
+from .consts import CARS_BRANDS,TRANSMISSION_OPTIONS
+from users.models import Profile,Location
+from .utils import user_listing_path
 
 class Listing(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     seller = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    brand = models.CharField(max_length=24,choices=CARS_BRANDS,default=None)
+    model = models.CharField(max_length=17)
+    mileage = models.IntegerField(default=0)
+    color = models.CharField(max_length=17)
+    description = models.TextField()
+    engine = models.CharField(max_length=17)
+    transmission = models.CharField(max_length=17, choices = TRANSMISSION_OPTIONS, default=None)
+    location = models.OneToOneField(Location,on_delete=models.SET_NULL,null=True)
+    image = models.ImageField(upload_to=user_listing_path)
