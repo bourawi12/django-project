5eba3a8907ae91bcee1c9789ba496d6f69917449
bourawi12 <mohamed12bouaziz@gmail.com>
2024-09-21 01:46:58 +0100

    adding listing data

--- src/main/models.py
+++ src/main/models.py
@@ -18,3 +18,6 @@ class Listing(models.Model):
     transmission = models.CharField(max_length=17, choices = TRANSMISSION_OPTIONS, default=None)
     location = models.OneToOneField(Location,on_delete=models.SET_NULL,null=True)
     image = models.ImageField(upload_to=user_listing_path)
+
+    def __str__(self):
+        return f'{self.seller.user.username}\'s Listing - {self.model}'
