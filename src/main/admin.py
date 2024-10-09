94ae8c670d8e7f04807ae7b72e9a7a9be03bfa4c
bourawi12 <mohamed12bouaziz@gmail.com>
2024-10-09 22:45:45 +0100

    create liked listing model

--- src/main/admin.py
+++ src/main/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
-from .models import Listing
+from .models import Listing, LikedListing
+
 
 class ListingAdmin(admin.ModelAdmin):
-    readonly_fields = ('id',)
+    readonly_fields = ('id', )
 
-admin.site.register(Listing,ListingAdmin)
+
+class LikedListingAdmin(admin.ModelAdmin):
+    readonly_fields = ('id', )
+
+
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(LikedListing, LikedListingAdmin)
